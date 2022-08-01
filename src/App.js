e7dfda602f3f740c78a44836767a4bc6956e157d
andrustn85 <rustandres@gmail.com>
2022-08-01 14:32:42 +0200

    practica

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import cars from './practice'
+
+const [honda, tesla] = cars
+
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla
+
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda
+
+const {coloursByPopularity: [teslaTopColour]} = tesla
+
+const {coloursByPopularity: [hondaTopColour]} = honda
+  
+  function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <table>
+     <tr>
+       <th> Brand </th>
+       <th> Top Speed </th>
+     </tr>
+     <tr>
+       <td>{tesla.model}</td>
+       <td>{teslaTopSpeed}</td>
+       <td>{teslaTopColour}</td>
+     </tr>
+     <tr>
+       <td>{honda.model}</td>
+       <td>{hondaTopSpeed}</td>
+       <td>{hondaTopColour}</td>
+     </tr>
+   </table>,
       </header>
     </div>
   );
